1e1caaa0db2c5be0cdbd6212272fd531bbb3c4a4
haqua <harthur96@gmail.com>
2024-12-25 17:05:32 -0500

    - Added input to the app

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ import numpy as np
 st.title("Churn Prediction App")
 
 st.divider()
+
+st.write("Please enter the values and hit the prediction button to see the prediction")
+
+st.divider()
+
+age = st.number_input("Enter age", min_value=18, max_value=100, value=25)
+monthlycharge = st.number_input("Enter monthly charge", min_value=30, max_value=150)
+tenure = st.number_input("Enter tenure", min_value=0, max_value=130, value=10)
+gender = st.selectbox("Enter gender", ["Male", "Female"])
+
+st.divider()
